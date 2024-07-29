c5ba9f1db0a977aa905d8e5f25d6ae7485d40549
ryan <concepting@protonmail.com>
2024-07-29 19:46:22 -0400

    added functionality to add and remove items
    need to fix adding....

--- pantry-tracker/app/page.js
+++ pantry-tracker/app/page.js
@@ -1,10 +1,26 @@
 "use client"
 
+import {Box, Stack, Typography, Button, Modal, TextField} from '@mui/material';
 import { firestore } from '@/firebase';
-import {Box, Stack, Typography, Button} from '@mui/material';
-import { Firestore } from 'firebase/firestore';
-import { getDocs, collection } from 'firebase/firestore';
+// import { Firestore } from 'firebase/firestore';
+import { doc, getDocs, setDoc, collection, query, deleteDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+};
 
 export default function Home() {
   const [pantry, setPantry] = useState([]);
@@ -13,19 +29,34 @@ export default function Home() {
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
+  const [itemName, setItemName] = useState('')
+
+  const updatePantry = async () =>  {
+    const snapshot = collection(firestore, 'pantry');
+    const docs = await getDocs(snapshot);
+    const pantryList = [];
+    docs.forEach(doc => {
+      pantryList.push(doc.id);
+    });
+    console.log(pantryList);
+    setPantry(pantryList);
+  }
+
   useEffect(() => {
-    const updatePantry = async () =>  {
-      const snapshot = collection(firestore, 'pantry');
-      const docs = await getDocs(snapshot);
-      const pantryList = [];
-      docs.forEach(doc => {
-        pantryList.push(doc.id);
-      });
-      console.log(pantryList);
-      setPantry(pantryList);
-    }
     updatePantry();
   }, [])
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    await setDoc(docRef, {})
+    updatePantry()
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    await deleteDoc(docRef)
+    updatePantry()
+  }
 
   return (
     <Box
@@ -37,35 +68,66 @@ export default function Home() {
       alignItems = {'center'}
       gap = {2}
     >
-      <Button variant="contained">Add</Button>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx = {style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width = "100%" direction = {'row'} spacing = {2}>
+            <TextField id="outlined-basic" label="Item" variant="outlined" fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)}/>
+            <Button variant="outlined">Search</Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Button variant="contained"
+        onClick={() => {
+          addItem(itemName)
+          setItemName('')
+          handleClose()
+        }}
+
+      >Add</Button>
+
       <Box border={'1px solid #333'}>
-      <Box width="800px" height="100px" bgcolor={'#ADD8E6'} display={'flex'} justifyContent={'center'} alignItems={'center'} border={'1px solid #333'}>
-        <Typography variant={'h2'} color={'#333'} textAlign={'center'} >
-          Pantry Items
-        </Typography>
-      </Box>
-      <Stack width = "800px" height = "300px" spacing= {2} overflow= {'auto'}>
-        {pantry.map((i) => (
-          <Box
-            key = {i}
-            bgcolor = {'#f0f0f0'}
-            color = {'black'}
-            display = {'flex'}
-            justifyContent = {'center'}
-            alignItems = {'center'}
-            width = {'100%'}
-            minHeight = {'150px'}
-          >
-            <Typography variant= {'h3'} color = {'#333'} textAlign = {'center'}>
-              {
-                // capitalize the first letter of the item
-                i.charAt(0).toUpperCase() + i.slice(1)
-              }
-            </Typography>
-          </Box>
-        ))
-        }
-      </Stack>
+        <Box width="800px" height="100px" bgcolor={'#ADD8E6'} display={'flex'} justifyContent={'center'} alignItems={'center'} border={'1px solid #333'}>
+          <Typography variant={'h2'} color={'#333'} textAlign={'center'} >
+            Pantry Items
+          </Typography>
+        </Box>
+
+        <Stack width = "800px" height = "300px" spacing= {2} overflow= {'auto'}>
+          {pantry.map((i) => (
+              <Box
+                key = {i}
+                bgcolor = {'#f0f0f0'}
+                color = {'black'}
+                display = {'flex'}
+                justifyContent = {'space-between'}
+                paddingX={2}
+                alignItems = {'center'}
+                width = {'100%'}
+                minHeight = {'150px'}
+              >
+                <Typography variant= {'h3'} color = {'#333'} textAlign = {'center'}>
+                  {
+                    // capitalize the first letter of the item
+                    i.charAt(0).toUpperCase() + i.slice(1)
+                  }
+                </Typography>
+
+                <Button variant="contained" onClick={() => removeItem(i)}>
+                  Remove
+                </Button>
+              </Box>
+          ))
+          }
+        </Stack>
       </Box>
     </Box>
   );
