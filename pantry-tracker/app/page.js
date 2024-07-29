b5f21b1c822dc1f310adb24a3e7e5ee71008f419
ryan <concepting@protonmail.com>
2024-07-29 18:14:22 -0400

    Update page.js

--- pantry-tracker/app/page.js
+++ pantry-tracker/app/page.js
@@ -1,4 +1,4 @@
-import {Box, Stack} from '@mui/material';
+import {Box, Stack, Typography} from '@mui/material';
 
 const item = [
   'tomato',
@@ -23,26 +23,39 @@ export default function Home() {
       width = "100vw"
       height = "100vh"
       display = {'flex'}
+      flexDirection = {'column'}
       justifyContent = {'center'}
       alignItems = {'center'}
     >
-      <Stack width = "800px" height= "200px" spacing= {2} overflow= {'auto'}>
+      <Box border={'1px solid #333'}>
+      <Box width="800px" height="100px" bgcolor={'#ADD8E6'} display={'flex'} justifyContent={'center'} alignItems={'center'} border={'1px solid #333'}>
+        <Typography variant={'h2'} color={'#333'} textAlign={'center'} >
+          Pantry Items
+        </Typography>
+      </Box>
+      <Stack width = "800px" height = "300px" spacing= {2} overflow= {'auto'}>
         {item.map((i) => (
           <Box
             key = {i}
-            bgcolor = {'primary.main'}
-            color = {'primary.contrastText'}
+            bgcolor = {'#f0f0f0'}
+            color = {'black'}
             display = {'flex'}
             justifyContent = {'center'}
             alignItems = {'center'}
             width = {'100%'}
-            height = {'100%'}
+            height = {'300px'}
           >
-            {i}
+            <Typography variant= {'h3'} color = {'#333'} textAlign = {'center'}>
+              {
+                // capitalize the first letter of the item
+                i.charAt(0).toUpperCase() + i.slice(1)
+              }
+            </Typography>
           </Box>
         ))
         }
       </Stack>
+      </Box>
     </Box>
   );
 }
